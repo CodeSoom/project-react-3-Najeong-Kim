50437c790c2eb8461e557497c141d831c4b1cb5e
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-04-06 00:15:42 +0900

    ChoicePage Button 디자인

--- src/styles/ChoiceButton.jsx
+++ src/styles/ChoiceButton.jsx
@@ -1,13 +1,16 @@
 import styled from '@emotion/styled';
 
 const ChoiceButton = styled.button({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
   padding: '20px',
   backgroundColor: 'white',
   color: '#828282',
   border: 'none',
   fontSize: '18px',
   textAlign: 'left',
-  height: '100px',
+  height: '150px',
   borderRadius: '10px',
   '&:hover': {
     boxShadow: '0px 0px 5px 1px rgba(0, 0, 0, 0.07)',
@@ -15,6 +18,15 @@ const ChoiceButton = styled.button({
   '&:focus': {
     outline: 'none',
   },
+  '& img': {
+    width: '120px',
+    height: '120px',
+    marginRight: '20px',
+    borderRadius: '10px',
+  },
+  '& p': {
+    backgroundColor: 'white',
+  }
 });
 
 export default ChoiceButton;
