f0cd14d890448e249481003a5d5c4384def3e4e9
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-04-06 17:13:36 +0900

    IntroPage, ResultPage에 이름 불러주는 문구 추가

--- src/ResultPage.jsx
+++ src/ResultPage.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+
+import { useSelector } from 'react-redux';
+
 import ResultContainer from './ResultContainer';
+
+import { get } from './utils';
+
 
 import Title from './styles/Title';
 import Text from './styles/Text';
 
 export default function ResultPage() {
+  const userName = useSelector(get('userName'));
   return (
     <div>
       <Title>Result</Title>
       <Text>
-        당신의 비타민 Y는 아래와 같습니다
+        {userName}님의 비타민 Y는 아래와 같습니다
       </Text>
       <ResultContainer />
       <Text>
