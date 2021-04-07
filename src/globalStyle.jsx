c336efcbeedb70e03e8d1e6e50463218c3e28ce5
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-04-07 12:56:11 +0900

    Stylish font 적용, box-sizing: border-box 적용

--- src/globalStyle.jsx
+++ src/globalStyle.jsx
@@ -4,8 +4,11 @@ import { Global, css } from '@emotion/react';
 export const GlobalStyle = () => (
   <Global
     styles={css`
+      @import url('https://fonts.googleapis.com/css2?family=Stylish&display=swap');
       * {
+        box-sizing: border-box;
         background-color: #FFFEF2;
+        font-family: 'Stylish', sans-serif;
       }
     `}
   />
