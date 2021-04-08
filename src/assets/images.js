d4d6127d561df44aabddd6e145eb8f5e05c7e2e1
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-04-08 17:12:45 +0900

    사라진 파일들 복구

---
+++ src/assets/images.js
@@ -0,0 +1,146 @@
+import bottles from './images/illustrations/bottles.png';
+
+import choiceImage1 from './images/choices/1-1.jpeg';
+import choiceImage2 from './images/choices/1-2.jpeg';
+import choiceImage3 from './images/choices/2-1.jpeg';
+import choiceImage4 from './images/choices/2-2.jpeg';
+import choiceImage5 from './images/choices/3-1.jpeg';
+import choiceImage6 from './images/choices/3-2.jpeg';
+import choiceImage7 from './images/choices/4-1.jpeg';
+import choiceImage8 from './images/choices/4-2.jpeg';
+import choiceImage9 from './images/choices/5-1.jpeg';
+import choiceImage10 from './images/choices/5-2.jpeg';
+import choiceImage11 from './images/choices/6-1.jpeg';
+import choiceImage12 from './images/choices/6-2.jpeg';
+import choiceImage13 from './images/choices/7-1.jpeg';
+import choiceImage14 from './images/choices/7-2.jpeg';
+import choiceImage15 from './images/choices/8-1.jpeg';
+import choiceImage16 from './images/choices/8-2.jpeg';
+import choiceImage17 from './images/choices/9-1.jpeg';
+import choiceImage18 from './images/choices/9-2.jpeg';
+
+import vitaminImage1 from './images/vitamins/1.jpeg';
+import vitaminImage2 from './images/vitamins/2.jpeg';
+import vitaminImage3 from './images/vitamins/3.jpeg';
+import vitaminImage4 from './images/vitamins/4.jpeg';
+import vitaminImage5 from './images/vitamins/5.jpeg';
+import vitaminImage6 from './images/vitamins/6.jpeg';
+import vitaminImage7 from './images/vitamins/7.jpeg';
+import vitaminImage8 from './images/vitamins/8.jpeg';
+import vitaminImage9 from './images/vitamins/9.jpeg';
+import vitaminImage10 from './images/vitamins/10.jpeg';
+import vitaminImage11 from './images/vitamins/11.jpeg';
+import vitaminImage12 from './images/vitamins/12.jpeg';
+import vitaminImage13 from './images/vitamins/13.jpeg';
+import vitaminImage14 from './images/vitamins/14.jpeg';
+import vitaminImage15 from './images/vitamins/15.jpeg';
+import vitaminImage16 from './images/vitamins/16.jpeg';
+import vitaminImage17 from './images/vitamins/17.jpeg';
+import vitaminImage18 from './images/vitamins/18.jpeg';
+import vitaminImage19 from './images/vitamins/19.jpeg';
+import vitaminImage20 from './images/vitamins/20.jpeg';
+import vitaminImage21 from './images/vitamins/21.jpeg';
+import vitaminImage22 from './images/vitamins/22.jpeg';
+import vitaminImage23 from './images/vitamins/23.jpeg';
+import vitaminImage24 from './images/vitamins/24.jpeg';
+import vitaminImage25 from './images/vitamins/25.jpeg';
+import vitaminImage26 from './images/vitamins/26.jpeg';
+import vitaminImage27 from './images/vitamins/27.jpeg';
+import vitaminImage28 from './images/vitamins/28.jpeg';
+import vitaminImage29 from './images/vitamins/29.jpeg';
+import vitaminImage30 from './images/vitamins/30.jpeg';
+import vitaminImage31 from './images/vitamins/31.jpeg';
+import vitaminImage32 from './images/vitamins/32.jpeg';
+import vitaminImage33 from './images/vitamins/33.jpeg';
+import vitaminImage34 from './images/vitamins/34.jpeg';
+import vitaminImage35 from './images/vitamins/35.jpeg';
+import vitaminImage36 from './images/vitamins/36.jpeg';
+import vitaminImage37 from './images/vitamins/37.jpeg';
+import vitaminImage38 from './images/vitamins/38.jpeg';
+import vitaminImage39 from './images/vitamins/39.jpeg';
+import vitaminImage40 from './images/vitamins/40.jpeg';
+import vitaminImage41 from './images/vitamins/41.jpeg';
+import vitaminImage42 from './images/vitamins/42.jpeg';
+import vitaminImage43 from './images/vitamins/43.jpeg';
+import vitaminImage44 from './images/vitamins/44.jpeg';
+import vitaminImage45 from './images/vitamins/45.jpeg';
+import vitaminImage46 from './images/vitamins/46.jpeg';
+import vitaminImage47 from './images/vitamins/47.jpeg';
+import vitaminImage48 from './images/vitamins/48.jpeg';
+
+export const illustrations = {
+  1: bottles,
+};
+
+
+export const choicesImages = {
+  1: choiceImage1,
+  2: choiceImage2,
+  3: choiceImage3,
+  4: choiceImage4,
+  5: choiceImage5,
+  6: choiceImage6,
+  7: choiceImage7,
+  8: choiceImage8,
+  9: choiceImage9,
+  10: choiceImage10,
+  11: choiceImage11,
+  12: choiceImage12,
+  13: choiceImage13,
+  14: choiceImage14,
+  15: choiceImage15,
+  16: choiceImage16,
+  17: choiceImage17,
+  18: choiceImage18,
+}
+
+export const vitaminsImages = {
+  1: vitaminImage1,
+  2: vitaminImage2,
+  3: vitaminImage3,
+  4: vitaminImage4,
+  5: vitaminImage5,
+  6: vitaminImage6,
+  7: vitaminImage7,
+  8: vitaminImage8,
+  9: vitaminImage9,
+  10: vitaminImage10,
+  11: vitaminImage11,
+  12: vitaminImage12,
+  13: vitaminImage13,
+  14: vitaminImage14,
+  15: vitaminImage15,
+  16: vitaminImage16,
+  17: vitaminImage17,
+  18: vitaminImage18,
+  19: vitaminImage19,
+  20: vitaminImage20,
+  21: vitaminImage21,
+  22: vitaminImage22,
+  23: vitaminImage23,
+  24: vitaminImage24,
+  25: vitaminImage25,
+  26: vitaminImage26,
+  27: vitaminImage27,
+  28: vitaminImage28,
+  29: vitaminImage29,
+  30: vitaminImage30,
+  31: vitaminImage31,
+  32: vitaminImage32,
+  33: vitaminImage33,
+  34: vitaminImage34,
+  35: vitaminImage35,
+  36: vitaminImage36,
+  37: vitaminImage37,
+  38: vitaminImage38,
+  39: vitaminImage39,
+  40: vitaminImage40,
+  41: vitaminImage41,
+  42: vitaminImage42,
+  43: vitaminImage43,
+  44: vitaminImage44,
+  45: vitaminImage45,
+  46: vitaminImage46,
+  47: vitaminImage47,
+  48: vitaminImage48,
+} 
