7dba305e6a10c330593bd433ff167674e23fea87
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-04-13 17:03:17 +0900

    IntroPage Mobile 디자인

--- src/pages/IntroPage.jsx
+++ src/pages/IntroPage.jsx
@@ -4,6 +4,8 @@ import { useSelector } from 'react-redux';
 
 import { Link } from 'react-router-dom';
 
+import { Desktop, Mobile } from '../MediaQuery'
+
 import styled from '@emotion/styled';
 
 import { get } from '../utils';
@@ -11,6 +13,7 @@ import { get } from '../utils';
 import { illustrations } from '../assets/images';
 
 import Container from '../styles/Container';
+import MobileContainer from '../styles/MobileContainer';
 
 const Start = styled.div({
   margin: '20px 0',
@@ -32,27 +35,56 @@ export default function IntroPage() {
   const userName = useSelector(get('userName'));
 
   return (
-    <Container>
-      <img src={illustrations[1]} alt="" />
-      <div>
-        <h2>Intro</h2>
-        <p className="introText">
-          안녕하세요 {userName}
-          님!
-          <br />
-          당신(You)만을 위한
-          <br />
-          비타민(Vitamin) 한 알을 준비했습니다
-          <br />
-          당신의 비타민 Y를 찾아보세요!
-        </p>
-        <Start><Link to="/choice/1">Start</Link></Start>
-        <p className="introPlusText">
-          비타민 Y는 당신의 삶에 행복을 주는 활동입니다
-          <br />
-          비타민 Y를 활동으로 생각하고 선택해주시면 됩니다
-        </p>
-      </div>
-    </Container>
+    <div>
+      <Desktop>
+        <Container>
+          <img src={illustrations[1]} alt="" />
+          <div>
+            <h2>Intro</h2>
+            <p className="introText">
+              안녕하세요 {userName}
+              님!
+              <br />
+              당신(You)만을 위한
+              <br />
+              비타민(Vitamin) 한 알을 준비했습니다
+              <br />
+              당신의 비타민 Y를 찾아보세요!
+            </p>
+            <Start><Link to="/choice/1">Start</Link></Start>
+            <p className="introPlusText">
+              비타민 Y는 당신의 삶에 행복을 주는 활동입니다
+              <br />
+              비타민 Y를 활동으로 생각하고 선택해주시면 됩니다
+            </p>
+          </div>
+        </Container>
+      </Desktop>
+      <Mobile>
+        <MobileContainer>
+          <h2>Intro</h2>
+          <div>
+            <p className="introText">
+              안녕하세요 {userName}
+              님!
+            </p>
+            <img src={illustrations[1]} alt="" />
+            <p className="introText">
+              당신(You)만을 위한
+              <br />
+              비타민(Vitamin) 한 알을 준비했습니다
+              <br />
+              당신의 비타민 Y를 찾아보세요!
+            </p>
+            <Start><Link to="/choice/1">Start</Link></Start>
+            <p className="introPlusText">
+              비타민 Y는 당신의 삶에 행복을 주는 활동입니다
+              <br />
+              비타민 Y를 활동으로 생각하고 선택해주시면 됩니다
+            </p>
+          </div>
+        </MobileContainer>
+      </Mobile>
+    </div>
   );
 }
