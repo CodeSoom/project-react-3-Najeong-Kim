fed485fd0bafd39e4bb81f96c2800e4149506503
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-04-16 17:17:32 +0900

    basename 공백

--- src/index.jsx
+++ src/index.jsx
@@ -14,7 +14,7 @@ import store from './store';
 ReactDOM.render(
   (
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="">
         <App />
       </BrowserRouter>
     </Provider>
