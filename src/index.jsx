8d4422070fdf94fbcedb32bfe7e5b53925f51b80
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2021-04-16 22:05:20 +0900

    basename 변경

--- src/index.jsx
+++ src/index.jsx
@@ -14,7 +14,7 @@ import store from './store';
 ReactDOM.render(
   (
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="vitamin-y.today">
         <App />
       </BrowserRouter>
     </Provider>
